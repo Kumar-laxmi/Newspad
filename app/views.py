170e1eae92c9160d4c3514e28ea63a8ec4a84a9d
Kumar-laxmi <laxmikantk2002@gmail.com>
2022-08-05 16:32:09 +0530

    News API working

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+import requests
+
+from newsapi import NewsApiClient
+
+API_KEY = '95bcdd235c90417cb39f254809479546'
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
+    url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    articles = data['articles']
+
+    context = {
+        'articles': articles
+    }
+
+    return render(request, 'index.html', context)
